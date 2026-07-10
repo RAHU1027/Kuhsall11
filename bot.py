83d0af1cf74e91567e9b8aa263ea83a78f2f9b8f
RAHU1027 <sanjitrai88888888@gmail.com>
2026-07-10 21:54:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from flask import Flask
 
 # --- CONFIG ---
 API_TOKEN = '5b108bd2fdd31c0c34bc65f24a5216a0'
-bot = telebot.TeleBot("8410119226:AAEDaMjNEmPINLbJc26RsPVNKgGjVNH_fSk")
+bot = telebot.TeleBot("8943344174:AAEnYcGFZCKyyZn0wcA_xuLw7mPCA4inRNw")
 ADMIN_ID = 8086760320  # <--- Yahan apni Telegram ID dalen
 PATH = "./" 
 file_id_cache = {} 
